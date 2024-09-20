6be79c43bad4464fcf6e743430c3469deea94f65
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-21 00:22:04 +0330

    feat: :sparkles: I added state and input handler for contact form
    I updated the `Contacts.jsx` file by adding a `useState` hook to manage the form's state.
    The initial state is set for the contact object with properties: `name`, `lastName`, `email`, and `phone`.
    I also implemented an `inputHandler` function to dynamically update the state based on user input, linking each input field to its corresponding state value.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
+
 import inputs from "../constants/inputs";
 import styles from "./Contacts.module.css";
 
 function Contacts() {
+  const [contact, setContact] = useState({ name: "", lastName: "", email: "", phone: "" });
+
+  const inputHandler = (event) => {
+    const { name, value } = event.target;
+    setContact((prevContact) => ({ ...prevContact, [name]: value }));
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.form}>
         {inputs.map((input, index) => (
-          <input
-            key={index}
-            type={input.type}
-            placeholder={input.placeholder}
-            name={input.name}
-          />
+          <input key={index} type={input.type} name={input.name} placeholder={input.placeholder} onChange={inputHandler} />
         ))}
         <button>Add Contact</button>
       </div>
