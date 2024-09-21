052960a11c09f9c251c6bdb729fdd0c6980e5d6a
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-21 11:24:07 +0330

    feat: :sparkles: I added ContactsList component with styling to display contacts
    I created the `ContactsList.jsx` component to display the contact list and styled it using `ContactsList.module.css`.
    The component uses `useContext` to access contacts from `ContactContext`.
    If there are contacts, they are rendered through the `ContactItem` component. Otherwise, a message is shown to indicate no contacts were found.
    The styles provide layout and presentation for the contact list and the message.

---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { ContactContext } from "../context/ContactProvider";
+
+import ContactItem from "./ContactItem";
+import styles from "./ContactsList.module.css";
+
+const ContactsList = () => {
+  const { contacts } = useContext(ContactContext);
+
+  return (
+    <div className={styles.container}>
+      <h3>Contacts List</h3>
+      {contacts.length ? (
+        <ul className={styles.contacts}>
+          {contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.message}>No contacts found</p>
+      )}
+    </div>
+  );
+};
+export default ContactsList;
